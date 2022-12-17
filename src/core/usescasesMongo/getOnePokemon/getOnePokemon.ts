f47e2beafb1ece86e3e639837de62910d5eeb236
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-12-17 14:57:15 +0100

    add gto one verification

--- src/core/usescasesMongo/getOnePokemon/getOnePokemon.ts
+++ src/core/usescasesMongo/getOnePokemon/getOnePokemon.ts
@@ -2,5 +2,8 @@ const { ObjectId } = require("mongodb");
 
 export const getOnePokemon = async(req: any, res: any, pokeCollection: any) => {
     const foundOnePokemon = await pokeCollection.findOne({"_id": ObjectId(req.params.id)})
+    if(foundOnePokemon == null) {
+        res.send('No pokemon have been founds')
+    }
     res.json(foundOnePokemon);
 }
